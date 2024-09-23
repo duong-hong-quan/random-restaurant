f77ce88b3fa3cd89d4ff5bbe52458c6a84c0d11d
Tuyen Van <59913038+TuyenVann@users.noreply.github.com>
2024-09-23 14:23:43 +0700

    split component

--- src/app/page.js
+++ src/app/page.js
@@ -67,6 +67,7 @@ import {
 import SpinHistory from "@/components/SpinHistory";
 import { LinkIcon, ShareIcon } from "lucide-react";
 import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import WheelCanvas from "@/components/WheelCanvas";
 
 export default function RandomWheel() {
   const canvasRef = useRef(null);
@@ -103,15 +104,6 @@ export default function RandomWheel() {
       timeout = setTimeout(() => func(...args), wait);
     };
   };
-
-  function randomHexColor() {
-    return (
-      "#" +
-      Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, "0")
-    );
-  }
 
   const spinWheel = () => {
     if (!locations?.length || locations?.length < 2) {
@@ -310,61 +302,7 @@ export default function RandomWheel() {
     }
   }, []);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    resizeCanvas();
-    drawWheel();
-
-    window.addEventListener("resize", resizeCanvas);
-    return () => window.removeEventListener("resize", resizeCanvas);
-
-    function resizeCanvas() {
-      const container = canvas.parentElement;
-      canvas.width = container.clientWidth;
-      canvas.height = container.clientHeight;
-      drawWheel();
-    }
-    function truncateText(text, maxWidth) {
-      let truncated = text;
-      while (ctx.measureText(truncated).width > maxWidth) {
-        truncated = truncated.slice(0, -1);
-      }
-      return truncated.length < text.length ? truncated + "..." : truncated;
-    }
-
-    function drawWheel() {
-      const centerX = canvas.width / 2;
-      const centerY = canvas.height / 2;
-      const radius = Math.max(Math.min(centerX, centerY) - 10, 0);
-      const totallocations = locations?.length;
-      if (totallocations === 0) return;
-      const arc = (Math.PI * 2) / totallocations;
-      for (let i = 0; i < totallocations; i++) {
-        const angle = i * arc;
-        ctx.beginPath();
-        ctx.fillStyle = randomHexColor();
-        ctx.moveTo(centerX, centerY);
-        ctx.arc(centerX, centerY, radius, angle, angle + arc);
-        ctx.lineTo(centerX, centerY);
-        ctx.fill();
-        ctx.save();
-        ctx.translate(centerX, centerY);
-        ctx.rotate(angle + arc / 2);
-        ctx.textAlign = "right";
-        ctx.fillStyle = "#333";
-        ctx.font = "bold 12px Arial";
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = 3;
-        const maxTextWidth = radius * 0.7;
-        const truncatedText = truncateText(locations[i], maxTextWidth);
-
-        ctx.strokeText(truncatedText, radius - 10, 5);
-        ctx.fillText(truncatedText, radius - 10, 5);
-        ctx.restore();
-      }
-    }
-  }, [locations]);
+  
   const handleClickOutside = (event) => {
     if (listRef.current && !listRef.current.contains(event.target)) {
       setListSuggestLocation([]);
@@ -414,7 +352,7 @@ export default function RandomWheel() {
         </Typography>
         <div className={`${styles.wheelContainer} `}>
           <div className={styles.selector}></div>
-          <canvas ref={canvasRef} className={styles.wheel}></canvas>
+          <WheelCanvas locations={locations} canvasRef={canvasRef} />
           <button
             className={styles.wheelCenter}
             onClick={spinWheel}
